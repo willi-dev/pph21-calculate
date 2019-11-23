cd0398c978d597f0982456b4de4858c0453eb0b0
willi-dev <willi.ilmukomputer@gmail.com>
2019-11-23 12:41:10 +0700

    init project: calculate pph21

--- src/App.js
+++ src/App.js
@@ -1,26 +1,143 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+/**
+ * baseTaxRelief
+ */
+export const baseTaxRelief = 54000000
+
+/**
+ * diffTaxRelief
+ */
+export const diffTaxRelief = 4500000
+
+/**
+ * taxStatus
+ */
+export const taxStatus = [
+  { status: 'TK0', description: 'Single' },
+  { status: 'K0', description: 'Married with no dependant' },
+  { status: 'K1', description: 'Married with 1 dependant' },
+  { status: 'K2', description: 'Married with 2 dependant' },
+  { status: 'K3', description: 'Married with 3 dependant' }
+]
+
+/**
+ * taxScheme
+ */
+export const taxScheme = [
+  { taxRate: 5/100,  maxLimit:  50000000 },
+  { taxRate: 15/100, maxLimit: 250000000 },
+  { taxRate: 25/100, maxLimit: 500000000 },
+  { taxRate: 30/100, maxLimit: 'none' }
+]
+
+/**
+ * getPKP
+ */
+export const getPKP = ({ statusIndex, perMonth}) => {
+  let PKP = 0
+  const perYear = perMonth * 12
+  const PKTP = baseTaxRelief + (diffTaxRelief * statusIndex)
+  if (perYear > PKTP) {
+    PKP = perYear - PKTP
+  }
+  return PKP
+}
+
+/**
+ * calculateTax
+ */
+export const calculateTax = (PKP) => {
+  let bottomLimit = 0
+  let incomeTax = 0
+  let remainIncome = PKP
+  taxScheme.forEach( (scheme, index) => {
+    if (remainIncome > (scheme.maxLimit - bottomLimit)) {
+      remainIncome = remainIncome - (scheme.maxLimit - bottomLimit)
+      incomeTax += scheme.taxRate * (scheme.maxLimit - bottomLimit)
+    } else {
+      incomeTax += scheme.taxRate * remainIncome
+      remainIncome = 0
+    }
+    bottomLimit = scheme.maxLimit
+  })
+  return incomeTax
+}
+
+/**
+ * App
+ * App component
+ */
+const App = () => {
+  const [status, setStatus] = useState(baseTaxRelief)
+  const [incomeMonth, setIncomeMonth] = useState(0)
+  const [incomeYear, setIncomeYear] = useState(0)
+  const [taxIncome, setTaxIncome] = useState(0)
+
+  const changeStatus = (e) => {
+    setStatus(e.target.value)
+  }
+
+  const changeIncome = (e) => {
+    setIncomeMonth(e.target.value)
+    setIncomeYear(e.target.value * 12)
+  }
+
+  const onClickCalculate = () => {
+    const params = {
+      statusIndex: status,
+      perMonth: incomeMonth
+    }
+    const PKP = getPKP(params)
+    const tax = calculateTax(PKP)
+    setTaxIncome(tax)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">setTaxIncome
+        <div className="col-12">
+          <h1>Perhitungan Pajak pph21</h1>
+        </div>
+      </div>
+      
+      <div className="row">
+        <div className="col-6">
+          <div className="form-group">
+            <label htmlFor="status">Pilih Status</label>
+            <select className="form-control" id="status" onChange={ (e) => changeStatus(e) }>
+              {
+                taxStatus.map((val, index) => {
+                  return (
+                    <option key={index} value={index}>{val.status} - {val.description}</option>
+                  )
+                })
+              }
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="income-per-month">Income Per Bulan</label>
+            <input id="income-per-month" type="text" className="form-control" value={incomeMonth} onChange={ (e) => changeIncome(e) }/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="income-per-year">Income Per Tahun</label>
+            <input id="income-per-year" type="text" className="form-control" value={incomeYear} disabled/>
+          </div>
+          <div className="form-group">
+            <button className="btn btn-primary" onClick={(e) => onClickCalculate()}>
+              Hitung Pajak
+            </button>
+          </div>
+
+          <h2>
+          Pajak: Rp. {taxIncome}
+          </h2>
+        </div>
+      </div>
+
     </div>
-  );
+  )
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App, {getPKP, calculateTax, taxStatus} from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('Tax Calculation', () => {
+  
+  /**
+   * get PKP with income 7.500.000 & status TK0
+   */
+  it('get PKP with income 7.500.000 & tax status TK0', () => {
+    const status = taxStatus.findIndex( tx => tx.status === 'TK0' )
+    const perMonth = 7500000
+    const params = {
+      statusIndex: status,
+      perMonth: perMonth
+    }
+    expect(getPKP(params)).toBe(36000000)
+  })
+
+  /**
+   * get PKP with income 4.500.000 & status K0
+   */
+  it('get PKP with income 4.500.000 & tax status K0', () => {
+    const status = taxStatus.findIndex( tx => tx.status === 'K0' )
+    const perMonth = 4500000
+    const params = {
+      statusIndex: status,
+      perMonth: perMonth
+    }
+    expect(getPKP(params)).toBe(0)
+  })
+
+  /**
+   * calculate tax with perMonth income 7500000 (PKP 36000000) & status K0
+   */
+  it('calculate tax with income 7.500.000 & tax status K0', () => {
+    const status = taxStatus.findIndex( tx => tx.status === 'K0' )
+    const perMonth = 7500000
+    const params = {
+      statusIndex: status,
+      perMonth: perMonth
+    }
+    const PKP = getPKP(params)
+    expect(calculateTax(PKP)).toBe(1575000)
+  })
+
+  /**
+   * calculate tax with perMonth inco
+  console.log(incomeTax)me 25000000 & status TK0
+   */
+  it('calculate tax with income 25.000.000 & tax status TK0', () => {
+    const status = taxStatus.findIndex( tx => tx.status === 'TK0' )
+    const perMonth = 25000000
+    const params = {
+      statusIndex: status,
+      perMonth: perMonth
+    }
+    const PKP = getPKP(params)
+    expect(calculateTax(PKP)).toBe(31900000)
+  })
+
+})
